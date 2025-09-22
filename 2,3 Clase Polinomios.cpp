ee23130d07c5d62e4bd8c0de59c091fce030a9b7
EmiPosse <posseemiliano2@gmail.com>
2025-09-22 12:46:57 -0300

    Update 2,3 Clase Polinomios.cpp

--- 2,3 Clase Polinomios.cpp
+++ 2,3 Clase Polinomios.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 /*Ejercicio 3
-Diseñe una clase Polinomio que contenga:
+DiseÃ±e una clase Polinomio que contenga:
 1. un constructor que reciba el grado el polinomio e inicialice sus coeficientes en 0.
-2. un método que permita cambiar un coeficiente.
-3. un método evaluar que permita evaluar el polinomio para un valor dado de la variable x.
-4. el/los métodos que considere necesarios para poder mostrar un polinomio desde un programa cliente.
-5. una función que permita sumar dos polinomios retornando un tercer polinomio con el resultado
-(ej. p_res = Sumar(p1, p2);). ¿Cómo variaría si en lugar de ser una función 
-libre fuera un método de la clase? Verifique el funcionamiento de la función Sumar(...) 
+2. un mÃ©todo que permita cambiar un coeficiente.
+3. un mÃ©todo evaluar que permita evaluar el polinomio para un valor dado de la variable x.
+4. el/los mÃ©todos que considere necesarios para poder mostrar un polinomio desde un programa cliente.
+5. una funciÃ³n que permita sumar dos polinomios retornando un tercer polinomio con el resultado
+(ej. p_res = Sumar(p1, p2);). Â¿CÃ³mo variarÃ­a si en lugar de ser una funciÃ³n 
+libre fuera un mÃ©todo de la clase? Verifique el funcionamiento de la funciÃ³n Sumar(...) 
 mediante un programa cliente
 */
 
 class Polinomio {
 private:
-	int *coef; // Array dinámico para los coeficientes
+	int *coef; // Array dinÃ¡mico para los coeficientes
 	int grado;
 	
 public:
 	// Constructor por defecto
-	Polinomio (int g): grado(g), coef(new int[grado+1]){ // creo un array dinámico con +1 por el T.I
+	Polinomio (int g): grado(g), coef(new int[grado+1]){ // creo un array dinÃ¡mico con +1 por el T.I
 	/// Inicializo todos los coeficientes en 0
 	for(int i=0 ; i<=grado ; i++) { 
 		*(coef+i) = 0;
