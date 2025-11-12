355c46445cecb65f318f8389ab6aef4d872564f8
EmiPosse <posseemiliano2@gmail.com>
2025-11-12 18:48:04 -0300

    Fix issues in binary file creation and reading

--- Unidad 6 - Archivos Binarios/6.1 Crear archivo binario con pares de num.cpp
+++ Unidad 6 - Archivos Binarios/6.1 Crear archivo binario con pares de num.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 /*
-a) Escriba a través de un programa C++ un archivo binario llamado grupo.dat, formado por un conjunto 
-de 200 pares de números generados aleatoriamente. Cada par de datos se conforma por un entero y flotante.
-b) Escriba otro programa que lea el archivo generado por el primero para verificar si se escribieron 
-correctamente.
+a) Escriba a través de un programa C++ un archivo binario llamado grupo.dat, 
+formado por un conjunto de 200 pares de números generados aleatoriamente.
+Cada par de datos se conforma por un entero y flotante.
+b) Escriba otro programa que lea el archivo generado por el primero para
+verificar si se escribieron correctamente.
 */
 
 struct Par{
@@ -15,27 +18,27 @@ struct Par{
 };
 
 int main(){
-	 srand(time(0));
+	srand(time(0));
 	string nombreArchivoBinario;
-	cout << "Ingrese el nombre del archivo binario que desea(con ): ";
+	cout << "Ingrese el nombre del archivo binario que desea(con la extension .dat): ";
 	cin >> nombreArchivoBinario;
 	fstream bin(nombreArchivoBinario, ios::binary | ios::out);
 	if(!bin.is_open()){ throw runtime_error("No se pudo abrir el archivo para escritura."); }
 	for(int i = 0; i < 200; i++){
 		Par p;
-		p.entero = 100 + rand() % 300;
-		p.flotante = 100.0 + rand() % 300 / 1.2345;
+		p.entero = 100 + rand() % (500 - 100 + 1);
+		p.flotante = 100.0 + rand() % (500 - 100 + 1);
 		bin.write((char*) &p, sizeof(p));
 	}
 	bin.close();
 	cout << "Se escribió el archivo con los 200 pares" << endl;
 	
-	fstream binw(nombreArchivoBinario, ios::binary | ios:: in);
-	if(!binw.is_open()){ throw runtime_error("No se pudo abrir el archivo para lectura."); }
+	fstream binr(nombreArchivoBinario, ios::binary | ios:: in);
+	if(!binr.is_open()){ throw runtime_error("No se pudo abrir el archivo para lectura."); }
 	int cantMostrar;
-	cout << "Ingrese la cantidad a mostrar: ":
+	cout << "Ingrese la cantidad a mostrar: ";
 	cin >> cantMostrar;
-	while(cantmostrar > 200){
+	while(cantMostrar > 200){
 		cout << "Cantidad de personas invalida, debe ser menor a 200, ingreselo de nuevo: ";
 		cin >> cantMostrar;
 	}
@@ -44,7 +47,7 @@ int main(){
 	while(binr.read(reinterpret_cast<char*>(&p), sizeof(Par))){
 		contador++;
 		if(contador <= cantMostrar){
-			cout "Entero numero: " << contador << endl;
+			cout << "Entero numero: " << contador << endl;
 			cout << p.entero << ", " << p.flotante << endl;
 		}
 	}	
