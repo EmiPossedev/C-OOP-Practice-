ac53e7ffa25ebaa90d9303e36b3bb89d5bb3d3fb
EmiPosse <posseemiliano2@gmail.com>
2025-11-12 09:26:42 -0300

    Crear 6.2 Modificar archivo binario.cpp

---
+++ Unidad 6 - Archivos Binarios/6.2 Modificar archivo binario.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+/*
+Escriba un programa abra el archivo generado en el ejercicio anterior y solicite al usuario que
+ingrese un entero, un flotante y una posición. El programa debe sobrescribir el par en la posición
+ingresada por el usuario por el nuevo par.
+Luego muestre la lista de datos en consola mostrando un par por línea.
+*/
+
+int main(){
+
+	
+
+	return 0;
+}
